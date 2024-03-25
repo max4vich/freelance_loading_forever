a2577b01eb30f0739fc2b69e543982c9fbf46618
max4vich <max777george@gmail.com>
2024-03-25 19:19:19 +0200

    Final? v3

--- src/ContactUs/ContactUs.js
+++ src/ContactUs/ContactUs.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 function ContactUs() {
     const messages = [
+        "Site is loading...",
         "Site is still loading...",
         "Loading assets....",
         "We are working on it....",
